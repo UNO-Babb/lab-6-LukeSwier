0ae24438daea55f7eba282c4cf0cfb88de251213
LukeSwier <lswier@unomaha.edu>
2025-10-05 18:10:53 +0000

    Done with diceroll

--- DiceRoll.py
+++ DiceRoll.py
@@ -8,11 +8,18 @@ def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+  for r in range(100):
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    rolls[dice1 + dice2 - 1] = rolls[dice1 + dice2 - 1] + 1
   #find the sum total of the two dice
   
   #print statictics for dice rolls
-
+  print("The sum of two dices 100 times")
+  dice = 1
+  for count in rolls:
+    print(dice,":", count)
+    dice = dice + 1
 
 if __name__ == '__main__':
   main()
